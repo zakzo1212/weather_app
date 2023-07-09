f9ecaed02204088980beed8285b133748c89d56d
Zachary Zhang <zakzo1212@gmail.com>
2023-07-09 00:36:50 -0500

    updated front end

--- script.js
+++ script.js
@@ -4,39 +4,46 @@ const apiKey = "17edb2ec3d374f8188cbb398663e5929";
 const locationElement = document.querySelector(".location");
 const temperatureElement = document.querySelector(".temperature");
 const conditionsElement = document.querySelector(".conditions");
+const fetchButton = document.getElementById("fetchButton");
 
-// Get user's location coordinates using Geolocation API
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+// Function to handle fetching weather data
+function fetchWeatherData() {
+  // Get user's location coordinates using Geolocation API
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-      // Make API request to Weatherbit
-      fetch(
-        `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${apiKey}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          // Extract the necessary weather data from the API response
-          const { city_name, country_code, temp, weather } = data.data[0];
+        // Make API request to Weatherbit Current Weather API
+        fetch(
+          `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Extract the necessary weather data from the API response
+            const { city_name, country_code, temp, weather } = data.data[0];
 
-          // Update the UI with the weather information
-          locationElement.textContent = `${city_name}, ${country_code}`;
-          temperatureElement.textContent = `${temp}°C`;
-          conditionsElement.textContent = weather.description;
-        })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
-          // Handle error fetching weather data
-        });
-    },
-    function (error) {
-      console.error("Error fetching user's location:", error.message);
-      // Handle error fetching user's location
-    }
-  );
-} else {
-  console.error("Geolocation is not supported by this browser.");
-  // Handle lack of geolocation support
+            // Update the UI with the weather information
+            locationElement.textContent = `${city_name}, ${country_code}`;
+            temperatureElement.textContent = `${temp}°C`;
+            conditionsElement.textContent = weather.description;
+          })
+          .catch((error) => {
+            console.error("Error fetching weather data:", error);
+            // Handle error fetching weather data
+          });
+      },
+      function (error) {
+        console.error("Error fetching user's location:", error.message);
+        // Handle error fetching user's location
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+    // Handle lack of geolocation support
+  }
 }
+
+// Event listener for the fetch button
+fetchButton.addEventListener("click", fetchWeatherData);
